e10b38dd4a7d0c916256851884a2d40952155f3f
unknown <6thform@6thform-51.st-andrews.ac>
2015-11-25 09:43:17 +0700

    Week2-2 Start

--- Week2/js/add-content.js
+++ Week2/js/add-content.js
@@ -1,10 +1,13 @@
-var price;
-var quantity;
-var total;
+var products=["Stamford T-Shirt", "Stamford Notebook", "Stamford Wristband"];
+var price=[10,5,1];
+var total = 0;
 
-price = 5;
-quantity = 14;
-total = price*quantity;
+var productList = document.getElementById("productList");
+productList.innerHTML += "<li>" +products[0] + ": $" + price[0] +"</li>" + "<li>" + products[1] + ": $" + price[1] + "</li>" + "<li>" + products[2]+ ": $" + price[2] + "</li>";
+
+
+
+total = price[0];
 
 //document.write("Total is " + total);
 var totalPriceEle = document.getElementById("totalPrice");
