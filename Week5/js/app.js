10955416b82d103f54c5fa06d6e4558616e62bc8
unknown <6thform@6thform-51.st-andrews.ac>
2015-12-23 05:22:23 +0700

    Week 4 Wrap up + Week 5 Publishing

--- Week5/js/app.js
+++ Week5/js/app.js
@@ -35,7 +35,7 @@ $(function(){
 					});
 					//function end
 					
-					
+					// check form for invalid phone number vvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvv
 					$("#form-submit").on("click",function(){
 					var phone = $("#form-number").val();
 						if(!$.isNumeric(phone)){
@@ -43,14 +43,74 @@ $(function(){
 							}
 							else{
 								$(".error:first").hide();
-								}				
+								}	
+						// checck form for invalid phone number	^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+						var msg = $("#form-message").val();
+			//using indexOf to not equal [-1] in order to find whether String exists in form or not
+							if(msg.indexOf("bad words") != -1){
+								$(".error:last").show();
+							}	else{
+								$(".error:last").hide();
+								}
+					
+						
 						}
 					);
+					
+					
+					
+					//flip function
 					$(".image-flip").flip(
 					{axis:'y',
 					trigger:'hover'
 						}
 					)
+					//flip function
+					
+					//image slider function ---------------------------------------------------------------------------------
+					
+					 jQuery(document).ready(function ($) {
+            
+            var jssor_1_SlideshowTransitions = [
+              {$Duration:1200,$Opacity:2}
+            ];
+            
+            var jssor_1_options = {
+              $AutoPlay: true,
+              $SlideshowOptions: {
+                $Class: $JssorSlideshowRunner$,
+                $Transitions: jssor_1_SlideshowTransitions,
+                $TransitionsOrder: 1
+              },
+              $ArrowNavigatorOptions: {
+                $Class: $JssorArrowNavigator$
+              },
+              $BulletNavigatorOptions: {
+                $Class: $JssorBulletNavigator$
+              }
+            };
+            
+            var jssor_1_slider = new $JssorSlider$("jssor_1", jssor_1_options);
+            
+            //responsive code begin
+            //you can remove responsive code if you don't want the slider scales while window resizing
+            function ScaleSlider() {
+                var refSize = jssor_1_slider.$Elmt.parentNode.clientWidth;
+                if (refSize) {
+                    refSize = Math.min(refSize, 600);
+                    jssor_1_slider.$ScaleWidth(refSize);
+                }
+                else {
+                    window.setTimeout(ScaleSlider, 30);
+                }
+            }
+           		 ScaleSlider();
+           				 $(window).bind("load", ScaleSlider);
+           				 $(window).bind("resize", ScaleSlider);
+          				  $(window).bind("orientationchange", ScaleSlider);
+          				  //responsive code end
+       				 });
+   					//image slider function-----------------------------------------------
 		}
 		
 //init event end
@@ -59,4 +119,5 @@ $(function(){
 
 	};
 	app.init();
+	
 });
